c76bce84bcd3e505bed294bd97f918b5388d1893
aatifshah <aatif.de.shah@gmail.com>
2017-01-22 01:12:12 -0800

    More more stuff

--- app/src/main/java/layout/TranscriptMainFragment.java
+++ app/src/main/java/layout/TranscriptMainFragment.java
@@ -1,7 +1,10 @@
 package layout;
 
+import android.app.Activity;
 import android.content.Context;
+import android.net.ConnectivityManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
@@ -15,7 +18,19 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -26,24 +41,35 @@ import mohammadsharif.com.spiik.R;
 import mohammadsharif.com.spiik.TranscriptActivity;
 
 public class TranscriptMainFragment extends Fragment implements View.OnClickListener, TextToSpeech.OnInitListener {
-    private Button transcript_translate;
+    private Button transcript_translate, transcript_play;
+    private TextView transcript_original_text;
     private TextToSpeech tts;
     private Spinner spinner;
     private HashMap<String, Locale> localeMap;
+    private TranscriptActivity parentActivity;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_transcript_main, container, false);
 
+        parentActivity = ((TranscriptActivity)getActivity());
+
         transcript_translate = (Button) view.findViewById(R.id.transcript_translate_button);
+        transcript_play = (Button) view.findViewById(R.id.transcript_play_button);
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+        transcript_original_text = (TextView) view.findViewById(R.id.transcript_original_text);
+
+        //setOnclickListeners
         transcript_translate.setOnClickListener(this);
+        transcript_play.setOnClickListener(this);
 
         tts = new TextToSpeech(this.getActivity().getBaseContext(), this);
-        spinner = (Spinner) view.findViewById(R.id.spinner);
 
         //Check if connected to the internet
-       
+        if(!parentActivity.isConnected()){
+            transcript_original_text.setText("Oops! Something went wrong.. No Internet Connectivity");
+        }
         return view;
     }
 
@@ -51,6 +77,9 @@ public class TranscriptMainFragment extends Fragment implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.transcript_translate_button:
+                //call translate api and pass to addtranslatefragment
+                TranslateText request = new TranslateText(transcript_original_text.getText().toString(), "en", getSpinnerLanguage());
+                request.execute();
                 ((TranscriptActivity)getActivity()).addTranslateFragment();
                 break;
             case R.id.transcript_play_button:
@@ -106,4 +135,123 @@ public class TranscriptMainFragment extends Fragment implements View.OnClickList
         String text = "hello my name is arjun";
         tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
     }
+
+    private String getSpinnerLanguage(){
+        String defLang = spinner.getSelectedItem().toString();
+        Locale loc = localeMap.get(defLang);
+        Log.v("TEST", loc.getLanguage());
+
+        return loc.getLanguage();
+    }
+
+    private class TranslateText extends AsyncTask<Void, Void, String> {
+        private String readText, sourceLang, targetLang;
+        final String TRANSLATE_BASE_URL = "https://translation.googleapis.com/language/translate/v2?";
+        final String TRANSLATE_API_KEY = "key";
+        final String SOURCE_LANG = "source";
+        final String TARGET_LANG = "target";
+        final String READ_TEXT = "q";
+
+        public TranslateText (String text, String sLang, String tLang ){
+            super();
+            readText = text;
+            sourceLang = sLang;
+            targetLang = tLang;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                Uri.Builder builder = new Uri.Builder();
+                builder.scheme("https")
+                        .authority("translation.googleapis.com")
+                        .appendPath("language")
+                        .appendPath("translate")
+                        .appendPath("v2")
+                        .appendQueryParameter(TRANSLATE_API_KEY, "AIzaSyDG12FxjQPxtgoe3sKLv-meHkkYlZQT4YM")
+                        .appendQueryParameter(SOURCE_LANG, sourceLang)
+                        .appendQueryParameter(TARGET_LANG, targetLang)
+                        .appendQueryParameter(READ_TEXT, readText)
+                        .build();
+                Log.v("TEST", builder.toString());
+                URL url = new URL(builder.toString());
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+
+                if(urlConnection.getInputStream() == null){
+                    return "Sorry, cannot translate this language.";
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.  + "\n"
+                    buffer.append(line);
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                return buffer.toString();
+            } catch (IOException e) {
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return "Sorry, cannot translate this language.";
+
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+        }
+
+
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                JSONObject translatedJSON = new JSONObject(s);
+                parentActivity.passTranslatedJSON( translatedJSON);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            transcript_original_text.setText(s);
+            Log.i("json", s + "d");
+        }
+
+    }
+
+
+
+
+
 }
--- app/src/main/java/layout/TranscriptTranslateFragment.java
+++ app/src/main/java/layout/TranscriptTranslateFragment.java
@@ -9,19 +9,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.json.JSONObject;
 
 import mohammadsharif.com.spiik.R;
+import mohammadsharif.com.spiik.TranscriptActivity;
 
 
-public class TranscriptTranslateFragment extends Fragment{
-    private Button transcript_translate;
+public class TranscriptTranslateFragment extends Fragment implements View.OnClickListener{
+    private ImageButton transcript_translate_cancel_button;
+    private TextView transcript_translated_text;
+    private JSONObject translatedJSON;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_transcript_translate, container, false);
 
+        transcript_translated_text = (TextView) view.findViewById(R.id.transcript_translated_text);
+        transcript_translate_cancel_button = (ImageButton) view.findViewById(R.id.transcript_translate_cancel_button);
+        transcript_translate_cancel_button.setOnClickListener(this);
         return view;
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.transcript_translate_cancel_button:
+                ((TranscriptActivity)getActivity()).killTranslateFragment();
+                break;
+        }
+    }
+
+    public void setTranslatedJSON(JSONObject tJSON){
+        //set JSON
+        translatedJSON = tJSON;
+
+        //display on text view
+        transcript_translated_text.setText(tJSON.toString());
+
+    }
 }
